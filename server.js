8f4de4ddb5d3078bd7ea7d475baa7db0348c8d45
VINOTH179 <vinothmagesh9@gmail.com>
2024-06-01 10:40:28 +0530

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const cors = require("cors");
 const UserModel = require('./models/User');
 const TicketModel = require('./models/Ticket');
-const bcrypt = require('bcryptjs');
-
 
 const app = express();
 app.use(express.json());
@@ -19,58 +16,43 @@ db.once('open', function() {
 });
 
 // User login endpoint
-app.post("/login", async (req, res) => {
+app.post("/login", (req, res) => {
     const { email, password } = req.body;
-    try {
-        const user = await UserModel.findOne({ email });
-        if (!user) {
-            return res.status(404).json("No user found with this email");
+    UserModel.findOne({ email })
+    .then(user => {
+        if (user) {
+            if (user.password === password) {
+                res.json("Success");
+            } else {
+                res.status(401).json("Incorrect password");
+            }
+        } else {
+            res.status(404).json("User not found");
         }
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json("Incorrect password");
-        }
-        res.json("Login successful");
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+    })
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
 // User registration endpoint
-app.post('/register', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const existingUser = await UserModel.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json("User already exists with this email");
-        }
-        const hashedPassword = await bcrypt.hash(password, 10); // saltRounds = 10
-        const newUser = await UserModel.create({ email, password: hashedPassword });
-        res.json(newUser);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+app.post('/register', (req, res) => {
+    UserModel.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
 // Get all booked tickets
-app.get('/booked-tickets', async (req, res) => {
-    try {
-        const tickets = await TicketModel.find({});
-        res.json(tickets);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+app.get('/booked-tickets', (req, res) => {
+    TicketModel.find({})
+    .then(tickets => res.json(tickets))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
 // Book tickets endpoint
-app.post('/book-tickets', async (req, res) => {
+app.post('/book-tickets', (req, res) => {
     const { movieName, theatreName, screenTime, seats, userName, ticketNumber } = req.body;
-    try {
-        const newTicket = await TicketModel.create({ movieName, theatreName, screenTime, seats, userName, ticketNumber });
-        res.json(newTicket);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+    TicketModel.create({ movieName, theatreName, screenTime, seats, userName, ticketNumber })
+    .then(ticket => res.json({ success: true, ticket }))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
 app.listen(5000, () => {
